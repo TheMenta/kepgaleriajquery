9ad7f3cdb5cf402e9d2cfddce0e5a4b02104698f
TheMenta <TheMentaaa@gmail.com>
2023-05-19 13:14:30 +0200

    biztonsagi mentes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,10 +3,38 @@ $(document).ready(function() {
     var images = [];
     $.getJSON("../assets/Data/data.json", function(data) {
         images = data.images;
-        console.log(images)
-      });
+        console.log(images);
+    });
     $(".jobb").click(function() {
         currentIndex = (currentIndex + 1) % images.length;
-        images++;
+        var currentImage = images[currentIndex].path;
+        $(".image-container").attr("src", currentImage);
+    });
+    $(".bal").click(function() {
+        currentIndex = (currentIndex - 1) % images.length;
+        var currentImage = images[currentIndex].path;
+        $(".image-container").attr("src", currentImage);
     });
 });
+$(document).ready(function() {
+    var smallImages = [];
+  
+    $.getJSON("../assets/Data/data.json", function(data) {
+      smallImages = data["small-images"];
+      displaySmallImages(smallImages);
+    });
+  
+    function displaySmallImages(images) {
+      var kiskepDiv = $(".kiskep");
+      for (var i = 0; i < images.length; i++) {
+        var imagePath = images[i].path;
+        var imageElement = $("<img>").addClass("image-container2").attr("src", imagePath).attr("alt", "kiskep");
+        kiskepDiv.append(imageElement);
+      }
+      var imageCount = images.length;
+      var colClass = "col-" + Math.floor(12 / imageCount);
+      $(".image-container2").addClass(colClass);
+    }
+  });
+  
+  

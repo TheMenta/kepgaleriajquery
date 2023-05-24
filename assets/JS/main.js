297683236b485f0e0b676017ac3a5f084e2d7a41
TheMenta <91569408+TheMenta@users.noreply.github.com>
2023-05-24 09:34:54 +0200

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,36 +5,43 @@ $(document).ready(function() {
         images = data.images;
         console.log(images);
     });
+    
     $(".jobb").click(function() {
         currentIndex = (currentIndex + 1) % images.length;
         var currentImage = images[currentIndex].path;
         $(".image-container").attr("src", currentImage);
     });
+    
     $(".bal").click(function() {
-        currentIndex = (currentIndex - 1) % images.length;
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
         var currentImage = images[currentIndex].path;
         $(".image-container").attr("src", currentImage);
     });
-});
-$(document).ready(function() {
+
     var smallImages = [];
-  
+
     $.getJSON("../assets/Data/data.json", function(data) {
-      smallImages = data["small-images"];
-      displaySmallImages(smallImages);
+        smallImages = data["small-images"];
+        kiskepek(smallImages);
     });
+
+    $(".kiskep").on("click", ".image-container2", function() {
+        var index = $(".image-container2").index(this);
+        var currentImage = images[index].path;
+        $(".image-container").attr("src", currentImage);
+    });
+    
   
-    function displaySmallImages(images) {
-      var kiskepDiv = $(".kiskep");
-      for (var i = 0; i < images.length; i++) {
-        var imagePath = images[i].path;
-        var imageElement = $("<img>").addClass("image-container2").attr("src", imagePath).attr("alt", "kiskep");
-        kiskepDiv.append(imageElement);
-      }
-      var imageCount = images.length;
-      var colClass = "col-" + Math.floor(12 / imageCount);
-      $(".image-container2").addClass(colClass);
-    }
   });
-  
-  
+    function kiskepek(images) {
+        var kiskepDiv = $(".kiskep");
+        for (var i = 0; i < images.length; i++) {
+            var imagePath = images[i].path;
+            var imageElement = $("<img>").addClass("image-container2").attr("src", imagePath).attr("alt", "kiskep");
+            kiskepDiv.append(imageElement);
+        }
+        var imageCount = images.length;
+        var colClass = "col-" + Math.floor(12 / imageCount);
+        $(".image-container2").addClass(colClass);
+    }
+
